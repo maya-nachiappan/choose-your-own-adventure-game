97fd4a26dd4914b9d2f96d6e3b80c5d125e3ba58
maya-nachiappan <maya.n102304@gmail.com>
2024-09-25 03:58:52 -0700

    initialized functions and used switch cases to manage player choices.

--- choose your own adventure game/main.cpp
+++ choose your own adventure game/main.cpp
@@ -6,9 +6,41 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void startGame(), firstChoice(), secondChoice(), thirdChoice(), fourthChoice(), fifthChoice(), sixthChoice(), seventhChoice(), eigthChoice(), ninthChoice(), tenthChoice(), endGame();
+
+
+int main()
+{
+    int score=0;
+    startGame();
     return 0;
+}
+
+void startGame()
+{
+    cout << "Welcome to your adventure game!" << endl;
+    cout << "You find yourself alone in an abandoned castle" << endl;
+    firstChoice();
+}
+
+void firstChoice()
+{
+    int choice;
+    cout << "Do you want to (1) start searching the castle or (2) search for weapons first?" << endl;
+    cin >> choice;
+    
+    switch (choice){
+        case 1:
+            cout << "You find a small sword" << endl;
+            break;
+        case 2:
+            cout << "You advance further into the castle, unarmed" << endl;
+            break;
+        default:
+            cout << "It is getting dark outside and you need to make a decision soon." << endl;
+            firstChoice();
+            break;
+    }
 }
