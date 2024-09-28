abd8aa62ee2bc4b95686b0afbc030888f80661e9
maya-nachiappan <maya.n102304@gmail.com>
2024-09-28 09:08:51 -0700

    changed first choice and added sub choices like forestChoice() and stayChoice(). also changed return types of choice functions

--- choose your own adventure game/main.cpp
+++ choose your own adventure game/main.cpp
@@ -6,77 +6,113 @@
 //
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <limits>
 using namespace std;
 
-void startGame(), firstChoice(), secondChoice(), thirdChoice(), fourthChoice(), fifthChoice(), sixthChoice(), seventhChoice(), eigthChoice(), ninthChoice(), tenthChoice(), endGame();
-
+void startGame();
+int forestChoice(), stayChoice();
+void firstChoice(), secondChoice(), thirdChoice(), fourthChoice(), fifthChoice(), sixthChoice(), seventhChoice(), eigthChoice(), ninthChoice(), tenthChoice(), endGame();
 
 int main()
 {
-    int score=0;
     startGame();
+    firstChoice();
     return 0;
 }
+    
+    void startGame()
+    {
+        cout << "Welcome to your adventure game!" << endl;
+        cout << "You find yourself alone in the woods, infront of an abandoned castle" << endl;
+    }
+    
+    void firstChoice()
+    {
+        int choice;
+        int score=0;
+        cout << "Do you want to (1) start searching the forest, (2) go towards the castle or (3) stay put and wait for other people to find you?" << endl;
+        cin >> choice;
+        
+        switch (choice){
+            case 1:
+                cout << "You begin to walk further into the forest" << endl;
+                score+=3;
+                forestChoice();
+                break;
+            case 2:
+                cout << "You advance further into the castle, unarmed" << endl;
+                score +=5;
+                break;
+            case 3:
+                cout << "You sit down on the ground and begin to wait and hope for people to find you" << endl;
+                score += 1;
+                break;
+            default:
+                cout << "It is getting dark and cold outside and you need to make a decision soon." << endl;
+                firstChoice();
+                break;
+        }
+    }
+    
 
-void startGame()
-{
-    cout << "Welcome to your adventure game!" << endl;
-    cout << "You find yourself alone in an abandoned castle" << endl;
-    firstChoice();
-}
 
-void firstChoice()
+// function if the user chooses to explore the forest
+int forestChoice()
 {
     int choice;
-    cout << "Do you want to (1) start searching the castle or (2) search for weapons first?" << endl;
+    cout << "You walk through the woods for a while, before eventually hearing the roar of a monster you have never heard before. do you (1) run back to the castle for shelter or (2) try to hide in the woods?" << endl;
     cin >> choice;
-    
-    switch (choice){
-        case 1:
-            cout << "You find a small sword" << endl;
-            break;
-        case 2:
-            cout << "You advance further into the castle, unarmed" << endl;
-            break;
-        default:
-            cout << "It is getting dark outside and you need to make a decision soon." << endl;
-            firstChoice();
-            break;
+    if (choice==1)
+    {
+        cout << "you manage to outrun the monster and reach the castle" << endl;
     }
-}
-void secondChoice()
-{
+    else if (choice==2)
+    {
+        cout << "you try to hide in the forest but are unable to outrun the monster and it catches you anyways. you die." << endl;
+        return 0;
+    }
     
 }
-void thirdChoice()
-{
+// function if the user chooses to stay put
+//void stayChoice()
+//{
     
-}
-void fourthChoice()
-{
+//}
+//int secondChoice()
+//{
     
-}
-void fifthChoice()
-{
+//}
+//int thirdChoice()
+//{
     
-}
-void sixthChoice()
-{
+//}
+//void fourthChoice()
+//{
     
-}
-void seventhChoice()
-{
+//}
+//void fifthChoice()
+//{
     
-}
-void eigthChoice()
-{
+//}
+//void sixthChoice()
+//{
     
-}
-void ninthChoice()
-{
+//}
+//void seventhChoice()
+//{
     
-}
-void tenthChoice()
-{
+//}
+//void eigthChoice()
+//{
     
-}
+//}
+//void ninthChoice()
+//{
+    
+//}
+//void tenthChoice()
+//{
+
+//}
