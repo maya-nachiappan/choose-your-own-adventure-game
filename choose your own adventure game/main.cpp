99135cefa076a4a94ae6a19b11d40846606891ad
maya-nachiappan <maya.n102304@gmail.com>
2024-09-29 23:18:39 -0700

    added second choice options, still need to fix score returns

--- choose your own adventure game/main.cpp
+++ choose your own adventure game/main.cpp
@@ -11,7 +11,7 @@
 #include <limits>
 using namespace std;
 
-  
+
 void startGame()
 {
         cout << "Welcome to your adventure game!" << endl;
@@ -22,21 +22,21 @@ int forestChoice(), stayChoice();
 
 int firstChoice()
 {
+    int score=0;
         int choice;
-        int score=0;
         cout << "Do you want to (1) start searching the forest, (2) go towards the castle or (3) stay put and wait for other people to find you?" << endl;
         cin >> choice;
         
         switch (choice){
             case 1:
                 cout << "You begin to walk further into the forest" << endl;
-                score+=3;
                 forestChoice();
+                score+= 3;
                 break;
             case 2:
                 cout << "You advance further into the castle, unarmed" << endl;
-                score +=5;
-                break;
+                score += 5;
+               break;
             case 3:
                 cout << "You sit down on the ground and begin to wait and hope for people to find you" << endl;
                 score += 1;
@@ -54,7 +54,8 @@ int firstChoice()
 // function if the user chooses to explore the forest
 int forestChoice()
 {
-    int score, choice;
+    int score=0;
+    int choice;
     cout << "You walk through the woods for a while, before eventually hearing the roar of a monster you have never heard before. do you (1) run back to the castle for shelter or (2) try to hide in the woods?" << endl;
     cin >> choice;
     if (choice==1)
@@ -76,10 +77,34 @@ int forestChoice()
 //{
     
 //}
-//int secondChoice()
-//{
+int secondChoice()
+{
+    int score;
+    int choice;
+    cout << "You walk through the castle and notice various weapons lying around. do you pick up the (1) bow and arrow, (2) the sword or (3) the gun?" << endl;
+    cin >> choice;
     
-//}
+    switch (choice){
+        case 1:
+            cout << "You pick up the bow and there are only 4 arrows " << endl;
+            score+= 3;
+            break;
+        case 2:
+            cout << "You pick up the sword and it is perfectly balanced and weighted in your hand" << endl;
+            score += 5;
+           break;
+        case 3:
+            cout << "You pick up the gun, but there is only one bullet" << endl;
+            score += 1;
+            break;
+        default:
+            cout << "you decide not to pick up any of the weapons and keep walking through the castle" << endl;
+            score -= 1;
+            break;
+    }
+return score;
+}
+    
 //int thirdChoice()
 //{
     
@@ -113,10 +138,13 @@ int forestChoice()
 
 //}
 
+
 int main()
 {
-    int totalScore=0;
+    int score=0;
     startGame();
     firstChoice();
+    secondChoice();
+    cout << "your total score is: " << score <<endl;
     return 0;
 }
