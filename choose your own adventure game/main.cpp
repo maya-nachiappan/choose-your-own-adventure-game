ee8b41c806196cb996eb20e0a146ae23e509ced8
maya-nachiappan <maya.n102304@gmail.com>
2024-09-29 22:47:31 -0700

    moved main function to the end of code, made first choice function return score value

--- choose your own adventure game/main.cpp
+++ choose your own adventure game/main.cpp
@@ -11,25 +11,17 @@
 #include <limits>
 using namespace std;
 
-void startGame();
-int forestChoice(), stayChoice();
-void firstChoice(), secondChoice(), thirdChoice(), fourthChoice(), fifthChoice(), sixthChoice(), seventhChoice(), eigthChoice(), ninthChoice(), tenthChoice(), endGame();
-
-int main()
+  
+void startGame()
 {
-    startGame();
-    firstChoice();
-    return 0;
-}
-    
-    void startGame()
-    {
         cout << "Welcome to your adventure game!" << endl;
         cout << "You find yourself alone in the woods, infront of an abandoned castle" << endl;
-    }
+}
     
-    void firstChoice()
-    {
+int forestChoice(), stayChoice();
+
+int firstChoice()
+{
         int choice;
         int score=0;
         cout << "Do you want to (1) start searching the forest, (2) go towards the castle or (3) stay put and wait for other people to find you?" << endl;
@@ -51,30 +43,34 @@ int main()
                 break;
             default:
                 cout << "It is getting dark and cold outside and you need to make a decision soon." << endl;
-                firstChoice();
+                firstChoice(); // the user must input a valid number or will be repeatedly prompted
                 break;
         }
-    }
+    return score;
+}
     
 
 
 // function if the user chooses to explore the forest
 int forestChoice()
 {
-    int choice;
+    int score, choice;
     cout << "You walk through the woods for a while, before eventually hearing the roar of a monster you have never heard before. do you (1) run back to the castle for shelter or (2) try to hide in the woods?" << endl;
     cin >> choice;
     if (choice==1)
     {
         cout << "you manage to outrun the monster and reach the castle" << endl;
+        score+=2;
     }
     else if (choice==2)
     {
         cout << "you try to hide in the forest but are unable to outrun the monster and it catches you anyways. you die." << endl;
         return 0;
+        
     }
-    
+    return score;
 }
+
 // function if the user chooses to stay put
 //void stayChoice()
 //{
@@ -116,3 +112,11 @@ int forestChoice()
 //{
 
 //}
+
+int main()
+{
+    int totalScore=0;
+    startGame();
+    firstChoice();
+    return 0;
+}
